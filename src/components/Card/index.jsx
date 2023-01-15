d57cda66695ea9d1ea81da24036ba10663009c87
Polzocr <micro.cpt.62@outlook.fr>
2023-01-15 10:26:18 +0100

    css desktop et mobile page accueil

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,17 @@
+import './index.css'
+import imageLoc from '../../assets/accueil/accueil-img.jpg'
+import { Link } from 'react-router-dom'
+
+function Card({title, cover, id}){
+    return(
+        <Link to={`/logement/${id}`} className='link-card'>
+            <article id='card'>
+                <img src={cover} alt='image location'></img>
+                <p>{title}</p>
+            </article>
+        </Link>
+       
+    )
+}
+
+export default Card
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,36 @@
-import Banner from "../../components/Banner"
+import { useEffect } from 'react'
 import imageAccueil from '../../assets/accueil/accueil-img.jpg'
+import './index.css'
+
+import Banner from "../../components/Banner"
+import Card from "../../components/Card"
+
+import data from '../../data/logements.json'
 
 function Home(){
+
+    // useEffect(() => {
+    //     async function fetchData(){
+    //         const response = await fetch('../../data/logements.json')
+    //         const data = await response.json()
+    //         console.log(data)
+    //     }
+    //     fetchData()
+    // }, [])
+    console.log(data)
     return (
         <main>
             <Banner title="Chez vous, partout et ailleurs" imageAccueil={imageAccueil}/>
+            <section id="cards">
+                {data.map((card) => {
+                    return <Card
+                            key={`${card.title}-${card.id}`}
+                            title={card.title}
+                            cover={card.cover} 
+                            id={card.id}
+                            />
+                })}
+            </section>
         </main>
     )
 }
