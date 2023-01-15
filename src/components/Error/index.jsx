126615102fafa8c33728e203b4ca8ddbbdf1923e
Polzocr <micro.cpt.62@outlook.fr>
2023-01-15 12:50:47 +0100

    css mobile et desktop page erro

--- src/components/Error/index.jsx
+++ src/components/Error/index.jsx
@@ -1,12 +1,15 @@
 import { Link } from "react-router-dom"
+import './index.css'
 
 function Error(){
     return (
-        <section>
-            <p>404</p>
-            <p>Oups! la page que vous demandez n'exiwste pas.</p>
-            <Link to="/">Retourner sur la page d'accueil</Link>  
-        </section>
+        <main>
+            <div className="error-page">
+                <p>404</p>
+                <p>Oups! la page que vous demandez n'existe pas.</p>
+                <Link to="/" className="error-page-link">Retourner sur la page d'accueil</Link>  
+            </div>
+        </main>
     )
 }
 
